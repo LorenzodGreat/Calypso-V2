59a607537da34ea5c2b1c37e085706d341c70684
LorenzodGreat <90816226+LorenzodGreat@users.noreply.github.com>
2022-10-26 19:16:11 -0500

    done

--- src/page/Intro.jsx
+++ src/page/Intro.jsx
@@ -4,13 +4,13 @@ import main from "../img/INTRO.mp4"
 export default function Intro() {
   return (
     <>
-      <div className='relative'>
+      {/* <div className='relative'>
         <div className='w-screen h-screen'>
-          <video className='w-full h-full object-cover' muted autoPlay controls='' >
+          <video className="absolute z-10 w-auto min-w-full min-h-full max-w-none" muted autoPlay controls='' >
             <source src={main} type="video/mp4" />
           </video>
         </div>
-        <div className='fixed inset-0 ml-[100vh] mt-[75vh] grid place-content-center'>
+        <div className='fixed inset-0 md:ml-[100vh] mt-[75vh] grid place-content-center'>
           <a
             class="inline-block hover:scale-125 hover:duration-300 rounded-full bg-gradient-to-r duration-300 from-violet-300 via-pink-500 to-sky-300 p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75"
             href="/Home"
@@ -22,7 +22,16 @@ export default function Intro() {
             </span>
           </a>
         </div>
-      </div>
+      </div> */}
+
+      <header class="relative flex items-center justify-center h-screen overflow-hidden">
+        <div class="relative z-30 p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
+          Welcome to my site!
+        </div>
+        <video className='absolute z-10 w-auto min-w-full min-h-full max-w-none' muted autoPlay controls=''>
+          <source src={main} type="video/mp4" />Your browser does not support the video tag.
+        </video>
+      </header>
     </>
   );
 }
