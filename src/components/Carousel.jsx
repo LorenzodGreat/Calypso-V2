d69e34597d6c5b4fc1977a33bf4337b0f9bcb460
LorenzodGreat <90816226+LorenzodGreat@users.noreply.github.com>
2022-11-04 09:19:09 -0500

    next

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -27,13 +27,13 @@ export default function CardsCarousel() {
         <img className='w-full shadow-xl relative' src={Slide1} alt="" />
         <div className='absolute md:flex lg:flex top-20 left-8 md:top-[10rem] md:left-[9rem] lg:top-[16rem] lg:left-[9rem]'>
           <div className='w-[20vh] sm:w-[80vh] lg:w-[80vh] md:w-[80vh] space-y-4'>
-            <div className='h-max lg:space-y-6 md:space-y-6 space-y-2'>
+            <div className='h-max lg:space-y-6 md:space-y-6'>
               <h1 className=' text-[#f9ac8d] font-semibold text-lg sm:text-6xl md:text-6xl lg:text-6xl'>Global Logistic</h1>
               <p className='text-white h-max text-[0.5rem] md:text-2xl lg:text-2xl sm:text-2xl font-base lg:pt-6'>Trusted way to transport all your goods from there to here.
                 Providing the most realiable, hassle-free, with full transparency, customer centric shipping experience.</p>
             </div>
             <div className='flex lg:pt-10 lg:space-x-8 md:space-x-8 sm:space-x-8 space-x-4'>
-              <a href="https://cal.tasoko.com/register"> <button className='text-white font-semibold w-[10vh] sm:w-[50vh] md:w-[50vh] lg:w-[50vh] text-[0.5rem] sm:text-base md:text-base lg:text-base  bg-gradient-to-tr from-[#f42888] via-[#c831bb87] to-[#8010f8] hover:scale-125 transition-all duration-300 hover:bg-[#8010f8] md:px-4 md:py-2 sm:px-4 sm:py-2 lg:px-4 lg:py-2 rounded-full border ring ring-[#8010f8]'>Register Now</button></a>
+              <a href="https://cal.tasoko.com/register"> <button className='text-white font-semibold w-[10vh] sm:w-[50vh] md:w-[50vh] lg:w-[50vh] text-[0.5rem] sm:text-base md:text-base lg:text-base bg-gradient-to-tr from-[#f42888] via-[#c831bb87] to-[#8010f8] hover:scale-125 transition-all duration-300 hover:bg-[#8010f8] md:px-4 md:py-2 sm:px-4 sm:py-2 lg:px-4 lg:py-2 rounded-full border ring ring-[#8010f8]'>Register Now</button></a>
             </div>
           </div>
         </div>
