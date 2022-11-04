ad696e224fc8bd6e1cc94214f6317dd5a5e47191
LorenzodGreat <90816226+LorenzodGreat@users.noreply.github.com>
2022-11-04 09:16:35 -0500

    Completed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -27,7 +27,7 @@ export default function CardsCarousel() {
         <img className='w-full shadow-xl relative' src={Slide1} alt="" />
         <div className='absolute md:flex lg:flex top-20 left-8 md:top-[10rem] md:left-[9rem] lg:top-[16rem] lg:left-[9rem]'>
           <div className='w-[20vh] sm:w-[80vh] lg:w-[80vh] md:w-[80vh] space-y-4'>
-            <div className='h-max space-y-6'>
+            <div className='h-max lg:space-y-6 md:space-y-6 space-y-2'>
               <h1 className=' text-[#f9ac8d] font-semibold text-lg sm:text-6xl md:text-6xl lg:text-6xl'>Global Logistic</h1>
               <p className='text-white h-max text-[0.5rem] md:text-2xl lg:text-2xl sm:text-2xl font-base lg:pt-6'>Trusted way to transport all your goods from there to here.
                 Providing the most realiable, hassle-free, with full transparency, customer centric shipping experience.</p>
@@ -42,7 +42,7 @@ export default function CardsCarousel() {
       <Carousel.Slide ><div className='relative'>
         <img className='w-full shadow-xl relative' src={Slide2} alt="" />
         <div className='absolute md:flex lg:flex top-20 left-8 md:top-[4rem] md:left-[9rem] lg:top-[4rem] lg:left-[9rem]'>
-          <div className='w-[30vh] sm:w-[80vh] lg:w-[80vh] md:w-[80vh] space-y-4'>
+          <div className='w-[25vh] sm:w-[80vh] lg:w-[80vh] md:w-[80vh] space-y-4'>
             <div>
               <img src={Low} className="w-[8vh] md:block lg:block sm:w-[50vh] md:w-[50vh] lg:w-[50vh]" alt="opps" />
             </div>
@@ -50,8 +50,8 @@ export default function CardsCarousel() {
               <p className='text-white h-max text-[0.5rem] md:text-2xl lg:text-2xl sm:text-2xl font-base'>Introductory rate low rate of <span className='font-bold text-yellow-600'>$3.50USD</span> Register today and receive 15% off your first shipment.</p>
             </div>
             <div className='flex lg:space-x-8 md:space-x-8 sm:space-x-8 space-x-4'>
-              <button className='text-white font-semibold w-[10vh] sm:w-[50vh] md:w-[50vh] lg:w-[50vh] text-[0.5rem] sm:text-base md:text-base lg:text-base  bg-gradient-to-tr from-[#f42888] via-[#c831bb87] to-[#8010f8] hover:scale-125 transition-all duration-300 hover:bg-[#8010f8] md:px-4 md:py-2 sm:px-4 sm:py-2 lg:px-4 lg:py-2 rounded-full border ring ring-[#8010f8]'><a href="/Rates">See Rates</a></button>
-              <button className='text-white font-semibold w-[10vh] sm:w-[50vh] md:w-[50vh] lg:w-[50vh] text-[0.5rem] sm:text-base md:text-base lg:text-base hover:scale-125 transition-all duration-300 md:px-4 md:py-2 sm:px-4 sm:py-2 lg:px-4 lg:py-2 rounded-full border ring ring-[#8010f8]'><a href="https://cal.tasoko.com/register">Register Now</a></button>
+              <button className='text-white font-semibold w-[10vh] sm:w-[50vh] md:w-[50vh] lg:w-[50vh] text-[0.45rem] sm:text-base md:text-base lg:text-base  bg-gradient-to-tr from-[#f42888] via-[#c831bb87] to-[#8010f8] hover:scale-125 transition-all duration-300 hover:bg-[#8010f8] md:px-4 md:py-2 sm:px-4 sm:py-2 lg:px-4 lg:py-2 rounded-full border ring ring-[#8010f8]'><a href="/Rates">See Rates</a></button>
+              <button className='text-white font-semibold w-[10vh] sm:w-[50vh] md:w-[50vh] lg:w-[50vh] text-[0.45rem] sm:text-base md:text-base lg:text-base hover:scale-125 transition-all duration-300 md:px-4 md:py-2 sm:px-4 sm:py-2 lg:px-4 lg:py-2 rounded-full border ring ring-[#8010f8]'><a href="https://cal.tasoko.com/register">Register Now</a></button>
             </div>
           </div>
         </div>
